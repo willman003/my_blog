cbce0ab7f942f17ecda376c044b5a0d7b0aa926a
Roarke <willman0031@gmail.com>
2020-02-10 17:05:28 +0700

    Edit Config.py, add debug, add testing

--- config.py
+++ config.py
@@ -22,6 +22,8 @@ class TestingConfig(Config):
     WTF_CSRF_ENABLED = False
 
 class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
 config = {
